79ee8613711ff39c86f27a0a819ff0dc3eeda99f
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-16 18:10:49 -0400

    update database.js - removed some comments

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-// Initial route and start db
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,7 +13,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
 
@@ -27,7 +27,7 @@ export const putDb = async (content) => {
   console.log('add to database: ', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
 
