76def515b1a31708693ccefc3c01c8542c456912
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-16 15:15:40 -0400

    update database.js - addled logic for content to add to db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+// Initial route and start db
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +14,16 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
-initdb();
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, text: content });
+
+  const result = await request;
+  console.log('add to database: ', result);
+}
+
